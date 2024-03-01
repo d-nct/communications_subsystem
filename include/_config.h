18a282092ae88e3b6847e16da7226e1311547872
Daniel Nocito <nctdaniel1@gmail.com>
2024-02-29 23:30:37 -0300

    Update _config.h com regras de negócio

--- include/_config.h
+++ include/_config.h
@@ -25,7 +25,7 @@
 #define LORA_PIN_SS               24 // Chip Select
 #define LORA_PIN_RST              25 // Reset
 #define LORA_PIN_DIO0             26 // Interrupt
-#define LORA_FREQUENCY            915E6
+#define LORA_FREQUENCY            915E6 // Hz, pode ser 433E6, 868E6 ou 915E6
 #define LORA_SYNC_WORD            0x53 // S, de SATS.    0-0xFF
 
 #ifdef ESP32
@@ -35,5 +35,11 @@
 #define SERIAL_BAUDRATE           9600
 #endif
 
+/* Regras de Negócio */
+/* ----------------- */
+#define LORA_MAX_PACKET_SIZE      255  // bytes
+#define LORA_MAX_RETRIES          3
+#define LORA_TIMEOUT              1000 // ms
+#define LORA_INTERVAL             1000 // ms
 
 #endif // DEFINITIONS_H
